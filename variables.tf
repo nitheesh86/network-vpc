4e345e69109807f12e0910bebd468b637ead07e2
Nitheesha Poojary <nitheesh86@gmail.com>
2021-04-28 21:46:08 +0100

    Adding enviournment variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
-locals {
-  profile_prefix = "development"
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
 }
+
+variable "env" {
+  description = "Name of enviournment"
+  type        = string
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
