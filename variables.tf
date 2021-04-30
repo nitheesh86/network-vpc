002f8bc123282c7db0b82a82ca3e6f1c3053be5c
Nitheesha Poojary <nitheesh86@gmail.com>
2021-04-30 19:15:59 +0100

    updating vatiables

--- variables.tf
+++ variables.tf
@@ -13,6 +13,4 @@ variable "env" {
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
   type        = string
-  default     = "0.0.0.0/0"
 }
-
