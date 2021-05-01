8fa840cc7a86c5fbf60be415bd3e61f79a862930
Nitheesha Poojary <nitheesh86@gmail.com>
2021-05-01 14:40:41 +0100

    adding vpc config

--- main.tf
+++ main.tf
@@ -12,13 +12,16 @@ provider "aws" {
 }
 
 module "network" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "app.terraform.io/nitheeshp/vpc/aws"
+  version = "0.0.1"
 
   name = var.name
   cidr = "10.0.0.0/16"
 
   azs            = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
