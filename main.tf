d3383420a69962b4111869bc419b735a475e185b
Nitheesha Poojary <nitheesh86@gmail.com>
2021-05-01 14:42:24 +0100

    adding vpc config

--- main.tf
+++ main.tf
@@ -20,8 +20,7 @@ module "network" {
 
   azs            = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
