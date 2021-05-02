38e271b636705189bdef2964b10fe9cefbb32f97
Nitheesha Poojary <nitheesh86@gmail.com>
2021-05-02 15:19:57 +0100

    adding vpc module from git

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 
 module "vpc" {
-  source = "github.com/nitheesh86/terraform-modules/modules/vpc?ref=v0.0.1"
+  source = "https://github.com/terraform-aws-modules/terraform-aws-vpc"
 
   name = var.name
   cidr = "10.0.0.0/16"
