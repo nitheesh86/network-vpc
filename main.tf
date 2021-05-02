5fbcc67dc999ba22b38cc3fc274251b83938547e
Nitheesha Poojary <nitheesh86@gmail.com>
2021-05-02 14:26:05 +0100

    updating workflow

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ module "vpc" {
   name = var.name
   cidr = "10.0.0.0/16"
 
-  azs            = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   enable_nat_gateway = true
