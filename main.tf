eb1dfe40cf853566f90d3e2a8fffca5ad94e296d
Nitheesha Poojary <nitheesh86@gmail.com>
2021-05-03 13:52:07 +0100

    updating vpc outouts

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "nitheeshp"
-    workspaces { prefix = "citi-" }
+    workspaces { prefix = "vpc-" }
   }
 }
 
